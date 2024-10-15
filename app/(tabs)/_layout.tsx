35b2c3ecde701c7bb544ba630ba5633e36bfc470
JohanLee2000 <94586878+JohanLee2000@users.noreply.github.com>
2024-10-15 17:36:44 -0400

    Set up tabs

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,39 @@
+import { Tabs } from 'expo-router';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+		tabBarActiveTintColor: '#ffd33d',
+		headerStyle: {
+		  backgroundColor: '#25292e',
+		},
+		headerShadowVisible: false,
+		headerTintColor: '#fff',
+		tabBarStyle: {
+		backgroundColor: '#25292e',
+		},      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="calendar"
+        options={{
+          title: 'Calendar',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'calendar-clear-sharp' : 'calendar-clear-outline'} color={color} size={24}/>
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
